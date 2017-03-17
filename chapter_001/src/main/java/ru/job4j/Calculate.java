0830b08d219ff55d78b997527d7d2aa3573b6145
Yura1232 <ntglhf@gmail.com>
2017-03-17 15:07:21 +0200

    task 4087

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,7 +1,16 @@
 package ru.job4j;
+/**
+ * Class Класс для вычисления арифметических операций +-/* 
+ * @autor ypak
+ * @since 17.03.2017
+ * @version 2
+*/
 public class Calculate{
-	public static void main(String[] args)
-	{
+	/**
+	* Конструктор, вывод строки в консоль.
+	* param args - args.
+	*/
+	public static void main(String[] args) {
 		System.out.println("Hello world");
 	}
 }
